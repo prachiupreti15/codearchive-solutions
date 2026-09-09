36ecf5218180c02fdaef4d0d768795268c290395
prachi upreti <prachiupreti2108@gmail.com>
2026-09-09 08:00:03 +0530

    Add solution: Count Commas in Range II

---
+++ LeetCode/count-commas-in-range-ii.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    long long countCommas(long long n) {
+        long long ans = 0;
+
+        for (long long a = 1000 ; a <= n ; a*=1000){
+            ans += n-a+1;
+        }
+        return ans;
+    }
+};
