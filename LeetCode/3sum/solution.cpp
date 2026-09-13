a82be8b8496fdf5ffd2a687cff1eae85c6f075a7
prachi upreti <prachiupreti2108@gmail.com>
2026-09-13 10:46:19 +0530

    Update solution: 3Sum

--- LeetCode/3sum/solution.cpp
+++ LeetCode/3sum/solution.cpp
@@ -1,6 +1,20 @@
-class Solution {
-public:
-    vector<vector<int>> threeSum(vector<int>& nums) {
-        
+   right--;
+
+                    // Skip duplicates for left
+                    while (left < right && nums[left] == nums[left - 1]) left++;
+
+                    // Skip duplicates for right
+                    while (left < right && nums[right] == nums[right + 1]) right--;
+                }
+                else if (sum < 0) {
+                    left++;
+                } 
+                else {
+                    right--;
+                }
+            }
+        }
+
+        return ans;
     }
 };
