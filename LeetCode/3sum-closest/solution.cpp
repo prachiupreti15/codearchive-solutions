27a7911461bc02d3e727d5b6d4e6ed844d4af73d
prachi upreti <prachiupreti2108@gmail.com>
2026-09-13 11:03:41 +0530

    Add solution: 3Sum Closest

---
+++ LeetCode/3sum-closest/solution.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int threeSumClosest(vector<int>& nums, int target) {
+        
+        int n = nums.size();
+        sort(nums.begin() , nums.end());
+
+        int closest = nums[0] + nums[1] + nums[2];
+
+        for (int i = 0; i < n - 2; i++) {
+            int left = i + 1;
+            int right = n - 1;
+
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+
+                if (abs(target - sum) < abs(target - closest)) {
+                    closest = sum;
+               
