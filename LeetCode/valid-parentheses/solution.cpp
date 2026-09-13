b4012be6977468c90cd8bf624f41533ecec54465
prachi upreti <prachiupreti2108@gmail.com>
2026-09-13 11:21:03 +0530

    Add solution: Valid Parentheses

---
+++ LeetCode/valid-parentheses/solution.cpp
@@ -0,0 +1,20 @@
+#include <stack>
+using namespace std;
+
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+
+        for(char ch : s) {
+            if(ch == '(' || ch == '{' || ch == '[') {
+                st.push(ch);
+            } 
+            else {
+                if(st.empty()) return false;
+
+                if((ch == ')' && st.top() == '(') ||
+                   (ch == '}' && st.top() == '{') ||
+                   (ch == ']' && st.top() == '[')) {
+                    st.pop();
+   
